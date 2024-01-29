fa2c0eb9bf10e52702e1774d31b2f98a245bc083
Тимур Шанашбаев <luckytima@gmail.com>
2024-01-29 22:28:59 +0300

    adnroid battery info rework

--- Assets/0_VRCM_4/Code/Sensors/BatterySensors.cs
+++ Assets/0_VRCM_4/Code/Sensors/BatterySensors.cs
@@ -13,18 +13,18 @@ public class BatterySensors
     {
         if (Application.platform == RuntimePlatform.Android)
         {
-            using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-            {
-                using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
-                {
-                    using (var androidPlugin = new AndroidJavaObject("spxyz.com.androidsensors.SensorsData", currentActivity))
-                    {
-                        return androidPlugin.Call<string>("GetBatteryPct");
-                    }
-                }
-            }
+            //using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            //{
+            //    using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            //    {
+            //        using (var androidPlugin = new AndroidJavaObject("spxyz.com.androidsensors.SensorsData", currentActivity))
+            //        {
+            //            return androidPlugin.Call<string>("GetBatteryPct");
+            //        }
+            //    }
+            //}
+            return $"{SystemInfo.batteryLevel}{44}";
         }
-
         return "nope";
     }
 }
